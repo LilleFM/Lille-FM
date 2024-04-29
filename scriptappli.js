334035615908f7f4b7f669c7abe715a8fa744226
miiforlong <135202760+miiforlong@users.noreply.github.com>
2024-04-29 11:35:01 +0200

    Add files via upload

---
+++ scriptappli.js
@@ -0,0 +1,22 @@
+function downloadAPK() {
+    // Remplacez "url_du_fichier.apk" par l'URL réelle de votre fichier APK sur le serveur
+    var url = "https://lillefm.github.io/Lille-FM/Lille_FM.apk";
+    
+    // Crée un élément <a> invisible
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    document.body.appendChild(a);
+  
+    // Définit l'URL du fichier à télécharger
+    a.href = url;
+  
+    // Définit le nom de fichier pour le téléchargement
+    a.download = 'Lille FM.apk';
+  
+    // Simule un clic sur l'élément <a> pour déclencher le téléchargement
+    a.click();
+  
+    // Supprime l'élément <a> du DOM
+    document.body.removeChild(a);
+  }
+  
